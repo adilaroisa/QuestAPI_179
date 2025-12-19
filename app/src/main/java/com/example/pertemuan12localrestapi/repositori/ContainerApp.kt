f5d0d87ac22084b5e8da9e6afcf9066e2e0b0cd7
adilaroisa <adilaroisa688@gmail.com>
2025-12-19 10:37:12 +0700

    mengkonfigurasi Retrofit dengan Logging Interceptor dan Kotlin Serialization

--- app/src/main/java/com/example/pertemuan12localrestapi/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/pertemuan12localrestapi/repositori/ContainerApp.kt
@@ -3,3 +3,26 @@ package com.example.pertemuan12localrestapi.repositori
 interface ContainerApp {
     val repositoryDataSiswa: RepositoryDataSiswa
 }
+
+class DefaultContainerApp : ContainerApp {
+    private val baseUrl = "http://10.0.2.2/umvTI/"
+
+    val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    val klien = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(
+            Json {
+                ignoreUnknownKeys = true
+                prettyPrint = true
+                isLenient = true
+            }.asConverterFactory("application/json".toMediaType())
+        )
+        .client(klien)
+        .build()

5b002d28eb4d6b1661e7a91cd0f2c4df0b5336d2
adilaroisa <adilaroisa688@gmail.com>
2025-12-19 15:34:22 +0700

    memperbaiki logika validasi input dan update state secara dinamis

--- app/src/main/java/com/example/pertemuan12localrestapi/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/pertemuan12localrestapi/viewmodel/EntryViewModel.kt
@@ -1,10 +1,25 @@
 package com.example.pertemuan12localrestapi.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan12localrestapi.modeldata.DetailSiswa
+import com.example.pertemuan12localrestapi.modeldata.UIStateSiswa
+import com.example.pertemuan12localrestapi.modeldata.toDataSiswa
+import com.example.pertemuan12localrestapi.repositori.RepositoryDataSiswa
+
 class EntryViewModel (private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
+    private set
 
-    private fun validasiInput(ui: DetailSiswa = uiStateSiswa.detailSiswa): Boolean =
-        ui.nama.isNotBlank() && ui.alamat.isNotBlank() && ui.telpon.isNotBlank()
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ):
+            Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
 
     fun updateUiState(detailSiswa: DetailSiswa) {
         uiStateSiswa = UIStateSiswa(detailSiswa, validasiInput(detailSiswa))

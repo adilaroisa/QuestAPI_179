847db7e081e23b06a48b7567c15a0999d44aeb01
adilaroisa <adilaroisa688@gmail.com>
2025-12-19 10:33:42 +0700

    menambahkan import

--- app/src/main/java/com/example/pertemuan12localrestapi/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/pertemuan12localrestapi/repositori/RepositoryDataSiswa.kt
@@ -1,5 +1,7 @@
 package com.example.pertemuan12localrestapi.repositori
 
+import com.example.pertemuan12localrestapi.modeldata.DataSiswa
+
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void>

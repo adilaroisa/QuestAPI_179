e4443f479f94f6d30dfae648de4302603300171d
adilaroisa <adilaroisa688@gmail.com>
2025-12-19 15:53:37 +0700

    menambahkan import di PenyediaViewModel

--- app/src/main/java/com/example/pertemuan12localrestapi/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan12localrestapi/viewmodel/provider/PenyediaViewModel.kt
@@ -1,6 +1,13 @@
 package com.example.pertemuan12localrestapi.viewmodel.provider
 
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pertemuan12localrestapi.repositori.AplikasiDataSiswa
+import com.example.pertemuan12localrestapi.viewmodel.EntryViewModel
+import com.example.pertemuan12localrestapi.viewmodel.HomeViewModel
 
 fun CreationExtras.aplikasiDataSiswa():AplikasiDataSiswa = (
         this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as

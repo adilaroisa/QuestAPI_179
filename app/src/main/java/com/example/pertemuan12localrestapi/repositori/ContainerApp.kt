16acd0b95b3d0456c3f4b4dfd1a74bcc39c407aa
adilaroisa <adilaroisa688@gmail.com>
2025-12-19 10:42:46 +0700

    menambahkan import di containerApp

--- app/src/main/java/com/example/pertemuan12localrestapi/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/pertemuan12localrestapi/repositori/ContainerApp.kt
@@ -1,5 +1,12 @@
 package com.example.pertemuan12localrestapi.repositori
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
 interface ContainerApp {
     val repositoryDataSiswa: RepositoryDataSiswa
 }

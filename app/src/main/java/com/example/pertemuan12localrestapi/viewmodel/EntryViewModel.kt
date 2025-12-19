378c96dac1769cdfb7998ac1cb21d364b2b08163
adilaroisa <adilaroisa688@gmail.com>
2025-12-19 15:23:00 +0700

    menambahkan fungsi validasi input dan updateUiState pada EntryViewModel

--- app/src/main/java/com/example/pertemuan12localrestapi/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/pertemuan12localrestapi/viewmodel/EntryViewModel.kt
@@ -3,5 +3,12 @@ package com.example.pertemuan12localrestapi.viewmodel
 class EntryViewModel (private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
 
+    private fun validasiInput(ui: DetailSiswa = uiStateSiswa.detailSiswa): Boolean =
+        ui.nama.isNotBlank() && ui.alamat.isNotBlank() && ui.telpon.isNotBlank()
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa = UIStateSiswa(detailSiswa, validasiInput(detailSiswa))
+    }
+
 
 }

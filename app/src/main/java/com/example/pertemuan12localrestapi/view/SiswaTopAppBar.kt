103a310ffecf0bdafff839ce23351759ff04ac1a
adilaroisa <adilaroisa688@gmail.com>
2025-12-19 15:57:38 +0700

    membuat komponen dasar SiswaTopAppBar dengan CenterAlignedTopAppBar

---
+++ app/src/main/java/com/example/pertemuan12localrestapi/view/SiswaTopAppBar.kt
@@ -0,0 +1,18 @@
+package com.example.pertemuan12localrestapi.view
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+
+}

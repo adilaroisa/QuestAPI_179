3c766462fbee3193826bacc852d3dc58e328feb2
adilaroisa <adilaroisa688@gmail.com>
2025-12-19 10:38:19 +0700

    mengimplementasi inisialisasi lazy untuk ServiceApi dan Repository

--- app/src/main/java/com/example/pertemuan12localrestapi/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/pertemuan12localrestapi/repositori/ContainerApp.kt
@@ -26,3 +26,12 @@ class DefaultContainerApp : ContainerApp {
         )
         .client(klien)
         .build()
+
+    private val retrofitService: ServiceApiSiswa by lazy {
+        retrofit.create(ServiceApiSiswa::class.java)
+    }
+
+    override val repositoryDataSiswa: RepositoryDataSiswa by lazy {
+        JaringanRepositoryDataSiswa(retrofitService)
+    }
+}

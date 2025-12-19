03463fb644c7dd323c030c85a7a4f25871cae1bd
adilaroisa <adilaroisa688@gmail.com>
2025-12-19 16:02:21 +0700

    menambahkan komponen UI OnLoading dan OnError untuk HomeScreen

---
+++ app/src/main/java/com/example/pertemuan12localrestapi/view/HalamanHome.kt
@@ -0,0 +1,26 @@
+package com.example.pertemuan12localrestapi.view
+
+
+
+@Composable
+fun OnLoading(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+@Composable
+fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = stringResource(R.string.gagal), modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}

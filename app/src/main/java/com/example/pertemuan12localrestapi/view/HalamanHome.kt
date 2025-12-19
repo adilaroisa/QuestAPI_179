a6ff5d946a29f42583bcdc13d0cefd7d1b2f1735
adilaroisa <adilaroisa688@gmail.com>
2025-12-19 16:03:53 +0700

    menambahkan import di HalamanHome

--- app/src/main/java/com/example/pertemuan12localrestapi/view/HalamanHome.kt
+++ app/src/main/java/com/example/pertemuan12localrestapi/view/HalamanHome.kt
@@ -1,6 +1,29 @@
 package com.example.pertemuan12localrestapi.view
 
-
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan12localrestapi.R
+import com.example.pertemuan12localrestapi.modeldata.DataSiswa
+import com.example.pertemuan12localrestapi.uicontroller.route.DestinasiHome
+import com.example.pertemuan12localrestapi.viewmodel.HomeViewModel
+import com.example.pertemuan12localrestapi.viewmodel.StatusUiSiswa
+import com.example.pertemuan12localrestapi.viewmodel.provider.PenyediaViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable

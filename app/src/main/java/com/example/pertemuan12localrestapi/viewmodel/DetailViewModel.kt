4783d84f17790b338dc0f6fbea9f91643ad7fa61
adilaroisa <adilaroisa688@gmail.com>
2025-12-26 09:43:11 +0700

    menambahkan fungsi hapusSatuSiswa dengan penanganan respon API

--- app/src/main/java/com/example/pertemuan12localrestapi/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/pertemuan12localrestapi/viewmodel/DetailViewModel.kt
@@ -31,3 +31,14 @@ class DetailViewModel(
             }
         }
     }
+
+    suspend fun hapusSatuSiswa() {
+        val resp: Response<Void> = repositoryDataSiswa.hapusSatuSiswa(idSiswa)
+
+        if (resp.isSuccessful) {
+            println("Sukses Hapus Data : ${resp.message()}")
+        } else {
+            println("Gagal Hapus Data : ${resp.errorBody()}")
+        }
+    }
+}

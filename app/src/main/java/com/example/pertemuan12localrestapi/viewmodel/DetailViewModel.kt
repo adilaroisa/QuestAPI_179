dc31f47ca95f28d029cd3bf44decd3bffd4383bb
adilaroisa <adilaroisa688@gmail.com>
2025-12-26 11:23:33 +0700

    menambahkan import di DetailViewModel.kt

--- app/src/main/java/com/example/pertemuan12localrestapi/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/pertemuan12localrestapi/viewmodel/DetailViewModel.kt
@@ -1,5 +1,19 @@
 package com.example.pertemuan12localrestapi.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan12localrestapi.modeldata.DataSiswa
+import com.example.pertemuan12localrestapi.repositori.RepositoryDataSiswa
+import com.example.pertemuan12localrestapi.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import retrofit2.Response
+import java.io.IOException
+
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: DataSiswa) : StatusUIDetail
     object Error : StatusUIDetail

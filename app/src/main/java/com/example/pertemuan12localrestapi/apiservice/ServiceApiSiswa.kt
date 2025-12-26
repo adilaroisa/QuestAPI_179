215e8339f63a4ca51aea5581d5d85720820c7a5b
adilaroisa <adilaroisa688@gmail.com>
2025-12-26 08:46:14 +0700

    menambah import di service api siswa

--- app/src/main/java/com/example/pertemuan12localrestapi/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/pertemuan12localrestapi/apiservice/ServiceApiSiswa.kt
@@ -2,8 +2,10 @@ package com.example.pertemuan12localrestapi.apiservice
 
 import com.example.pertemuan12localrestapi.modeldata.DataSiswa
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Query
 
 interface ServiceApiSiswa {

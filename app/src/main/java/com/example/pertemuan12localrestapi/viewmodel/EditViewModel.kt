437093c30e1f457ad93861a28f735678da333eb3
adilaroisa <adilaroisa688@gmail.com>
2025-12-26 09:34:07 +0700

    implementasi updateUiState dan logika validasi pada EditViewModel

--- app/src/main/java/com/example/pertemuan12localrestapi/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/pertemuan12localrestapi/viewmodel/EditViewModel.kt
@@ -15,3 +15,15 @@ class EditViewModel(
                 .toUiStateSiswa(true)
         }
     }
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa = UIStateSiswa(
+            detailSiswa = detailSiswa,
+            isEntryValid = validasiInput(detailSiswa)
+        )
+    }
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
